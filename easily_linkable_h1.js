df369699929e2f6e96559bc4ddf6fdaa609a9cf9
Axel Hodler <axel@hodler.co>
2018-08-05 10:36:57 +0200

    put everything into its own file

---
+++ easily_linkable_h1.js
@@ -0,0 +1,42 @@
+{
+  let template = document.createElement('template')
+  template.innerHTML = `
+    <style>
+        a {
+            visibility: hidden;
+            text-decoration:none;
+        }
+
+        h1:hover a {
+            visibility: visible;
+        }
+
+        h1 {
+            margin-bottom: 300px;
+        }
+    </style>
+
+    <h1>
+        <a>🔗</a>
+        <span></span>
+    </h1>
+`
+
+  class EasilyLinkableH1 extends HTMLElement {
+    constructor() {
+      super()
+      let templateContent = template.content
+      const shadowRoot = this.attachShadow({mode: 'open'})
+        .appendChild(templateContent.cloneNode(true))
+    }
+
+    connectedCallback() {
+      const title = this.getAttribute("title")
+      this.setAttribute('id', title)
+      this.shadowRoot.querySelector('h1 > a').setAttribute('href', `#${title}`)
+      this.shadowRoot.querySelector('h1 > span').innerHTML = title
+    }
+  }
+
+  customElements.define('ah-h1', EasilyLinkableH1)
+}
